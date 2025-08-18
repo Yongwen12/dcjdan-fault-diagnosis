2f5d830429d03e82b58807289682849eaa1b96a1
Yongwen12 <74136089+Yongwen12@users.noreply.github.com>
2025-08-18 19:32:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ else:
 
 if st.button("Start Training"):
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = train_dcjdan(num_classes=3).to(device)
+    model = train_dcjdan.to(device)
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
     ce_loss = nn.CrossEntropyLoss()
 
